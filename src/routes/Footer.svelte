f8e42f71837aa6bb8e29d25592eb06980469a1a0
Hazeline. <roshin006@gmail.com>
2026-02-20 21:40:46 +0530

    skeleton of website part 2

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -2,6 +2,7 @@
     <p>Hazel's Garden LLC, founded by Hazeline Nishad, All Rights Reserved, 2026©</p>
     <p>Business Address: 1209 Mountain Road Pl Ne, Ste N, Albuquerque, NM, 87110</p>
     <p>Incorporated in Albuquerque, New Mexico, on 12th February, 2026.</p>
+    <p>For any inquiries, please email hazel@hazels.garden.</p>
 </footer>
 
 <style>
